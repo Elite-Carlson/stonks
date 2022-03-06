34e5c463e03d1856bcac0689a2c8a0ff54369fc4
elitecarlson <elitecarlson@gmail.com>
2022-03-06 23:59:01 +0200

    login init

--- login.php
+++ login.php
@@ -101,17 +101,34 @@
 </head>
 <body>
     <div class="form">
-        <form action="">
+        <form action="login.script.php" method="POST">
             <center>
                 <img src="img/stock-exchange-app.png" alt=""><br><br>
                 <h3>Stock<span class="watch">Watch</span><span class="seperator"> | </span>Login</h3><br><br>
-                <input type="text" class="username" placeholder="Username"><br><br>
+                <input type="text" class="username" name="username" placeholder="Username"><br><br>
                 <div class="passworddiv">
-                    <input type="password" class="password" id="inputs" placeholder="Password">
+                    <input type="password" class="password" name="password" id="inputs" placeholder="Password">
                     <div class="eye" alt=""></div>
                 </div><br><br>
-                <input type="submit" class="btn"><br><br>
+                <input type="submit" name="submit" class="btn"><br><br>
                 <a href="signup.php" class="signup">Don't have an account <span>Sign Up</span></a>
+                <br><br>
+                <?php
+                /*Error handlers*/
+                $url = $_SERVER['REQUEST_URI'];
+                if (preg_match("/emptyfield/", $url)) {
+                  echo '<p style="color: red; font-family: sans-serif; font-size: 14px;">Some fields are empty</p>';
+                }
+                if (preg_match("/didntinputinfo/", $url)) {
+                  echo '<p style="color: red; font-family: sans-serif; font-size: 14px;">First Login Buddy</p>';
+                }
+                if (preg_match("/wrongcredentials/", $url)) {
+                  echo '<p style="color: red; font-family: sans-serif; font-size: 14px;">Incorrect password</p>';
+                }
+                if (preg_match("/usernotfound/", $url)) {
+                  echo '<p style="color: red; font-family: sans-serif; font-size: 14px;">No such user Exists</p>';
+                }
+                ?>
             </center>
         </form>
     </div>
